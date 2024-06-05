a67a00a7e02de3a9156acf5ab1dbb9412d96c9e0
So Takahashi <porco.s2107@gmail.com>
2024-06-05 21:36:42 +0900

    chore:　ファイル追加

---
+++ frontend/src/routes/user.lazy.tsx
@@ -0,0 +1,18 @@
+import { createLazyFileRoute } from "@tanstack/react-router";
+import UsersList from "@/components/User/List";
+import User from "@/components/User/Register";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/gql/client";
+
+export const Route = createLazyFileRoute("/user")({
+  component: usersList,
+});
+
+function usersList() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <UsersList />
+      <User />
+    </QueryClientProvider>
+  );
+}
