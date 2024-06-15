f239a0072cc4de6c1fab3df59990d474b0399959
So Takahashi <porco.s2107@gmail.com>
2024-06-15 16:53:44 +0900

    👷chore: 認証まわりを試す

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,13 +1,13 @@
-import UsersList from "./components/User/List";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { queryClient } from "./gql/client";
+// import UsersList from "./components/User/List";
+// import { QueryClientProvider } from "@tanstack/react-query";
+// import { queryClient } from "./gql/client";
 
-function App() {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <UsersList />
-    </QueryClientProvider>
-  );
-}
+// function App() {
+//   return (
+//     <QueryClientProvider client={queryClient}>
+//       <UsersList />
+//     </QueryClientProvider>
+//   );
+// }
 
-export default App;
+// export default App;
